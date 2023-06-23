46c4f622c23b7a5dbda2245850040732fbb64b20
nijimajohn <hirokikotera0211@gmail.com>
2023-06-23 16:29:58 +0900

    Dialog追加

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,12 @@
-
-import { type Todo, add } from '../features/todo/todoSlice';
-import { v4 as uuidv4 } from 'uuid';
-import { Button, Divider, InputBase, Paper } from '@mui/material';
-import { useDispatch } from 'react-redux';
-import { useState } from 'react';
+import { type Todo, add } from '../features/todo/todoSlice'
+import { v4 as uuidv4 } from 'uuid'
+import { Button, Dialog, DialogActions, DialogContent, TextField } from '@mui/material'
+import { useDispatch } from 'react-redux'
+import { useState } from 'react'
 
 export const Form = (): JSX.Element => {
-
-  const dispatch = useDispatch();
-  const ID = uuidv4();
+  const dispatch = useDispatch()
+  const ID = uuidv4()
 
   const addTodo = (content: string): void => {
     const newTodo: Todo = {
@@ -16,33 +14,58 @@ export const Form = (): JSX.Element => {
       content,
       isCompleted: false,
     }
-    if (content === "") return;
+    if (content === '') return
     dispatch(add(newTodo))
-    setContent("");
+    setContent('')
   }
-  const [content, setContent] = useState("");
+  const [content, setContent] = useState('')
+  const [open, setOpen] = useState(false)
 
+  const handleClickOpen = (): void => {
+    setOpen(true)
+  }
 
+  const handleClose = (): void => {
+    setOpen(false)
+  }
 
   return (
     <>
-      <Paper
-        component="form"
-        sx={{ p: '4px 4px', display: 'flex', alignItems: 'center', width: "50%" }}
-      >
-        <InputBase
-          multiline
-          sx={{ ml: 1, flex: 1 ,}}
-          placeholder="タスクを入力"
-          inputProps={{ 'aria-label': 'タスクを入力' }}
-          value={content}
-          onChange={e => { setContent(e.target.value); }}
-        />
-        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-        <Button onClick={() => { addTodo(content) }}>送信</Button>
-        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      </Paper>
+      <div>
+        <Button variant="outlined" onClick={handleClickOpen}>
+          Todoを入力する
+        </Button>
+        <Dialog open={open} onClose={handleClose}>
+          {/* dialogを大きくする */}
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Todoを入力してください"
+              type="email"
+              fullWidth
+              variant="standard"
+              multiline
+              rows={2}
+              value={content}
+              onChange={(e) => {
+                setContent(e.target.value)
+              }}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>キャンセル</Button>
+            <Button
+              onClick={() => {
+                addTodo(content)
+              }}
+            >
+              送信
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
     </>
   )
 }
-
